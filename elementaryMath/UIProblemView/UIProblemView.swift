63f1e126aeee540d397524bef7695fe7934a9ef4
Andrei Balandin <andrei.balandin@cibc.com>
2019-02-06 15:29:23 -0500

    problem view designable code

--- elementaryMath/UIProblemView/UIProblemView.swift
+++ elementaryMath/UIProblemView/UIProblemView.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
+@IBDesignable
 class UIProblemView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var contentView:UIView?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "UIProblemView", bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
 }
