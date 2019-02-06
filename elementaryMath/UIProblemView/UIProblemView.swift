30ad2c56b3f87424cf94ded43c3fd93755b49f9f
Andrei Balandin <andrei.balandin@cibc.com>
2019-02-06 15:16:14 -0500

    problem view layout

---
+++ elementaryMath/UIProblemView/UIProblemView.swift
@@ -0,0 +1,21 @@
+//
+//  UIProblemView.swift
+//  elementaryMath
+//
+//  Created by Andrei Balandin on 2019-02-06.
+//  Copyright © 2019 Andrei Balandin. All rights reserved.
+//
+
+import UIKit
+
+class UIProblemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
