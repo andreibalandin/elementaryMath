a4d73689e14dae108884dbd75b829b37bdab6985
Andrei Balandin <andrei.balandin@cibc.com>
2019-02-06 13:32:08 -0500

    Initial Commit

---
+++ elementaryMath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elementaryMath
+//
+//  Created by Andrei Balandin on 2019-02-06.
+//  Copyright © 2019 Andrei Balandin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ elementaryMathTests/elementaryMathTests.swift
@@ -0,0 +1,34 @@
+//
+//  elementaryMathTests.swift
+//  elementaryMathTests
+//
+//  Created by Andrei Balandin on 2019-02-06.
+//  Copyright © 2019 Andrei Balandin. All rights reserved.
+//
+
+import XCTest
+@testable import elementaryMath
+
+class elementaryMathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
