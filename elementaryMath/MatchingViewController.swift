3dc3a13ae5820c310d7469aabcb1cbac6e464ab7
Andrei Balandin <andrei.balandin@cibc.com>
2019-02-14 15:33:40 -0500

    matching: adding buttons in a grid

--- elementaryMath/MatchingViewController.swift
+++ elementaryMath/MatchingViewController.swift
@@ -12,12 +12,40 @@ class MatchingViewController: UIViewController, GameInitialization, GameControlD
     var name: String = ""
     var date: Date = Date()
     
+    private var buttons: [MatchingButton] = []
+    
     @IBOutlet weak var gameControl: ControlView!
     
     func reset(complexity: Int) {
+        print("match reset \(complexity)")
+        print("frame \(view.frame)")
         
+        let width = Int(view.frame.size.width)
+        let height = width
+        let yOffset = 200
+        let size = width/10
+        let padding = 10
+        
+        for x in stride(from:0, to:width, by:size) {
+            for y in stride(from:0, to:height-50, by:size) {
+                let buttonFrame = CGRect(x:x+padding, y:y+yOffset+padding, width:size-padding*2, height:size-padding*2)
+                let button = MatchingButton(frame: buttonFrame)
+                button.x = x
+                button.y = y
+                button.setTitle(String(Int.random(in: 1..<10)), for:.normal)
+                button.backgroundColor = UIColor.gray
+                button.layer.cornerRadius = 10
+                button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+                buttons.append(button)
+                view.addSubview(button)
+            }
+        }
     }
     
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+    }
+
     func verify() -> Int {
         return 0
     }
@@ -30,4 +58,10 @@ class MatchingViewController: UIViewController, GameInitialization, GameControlD
         
         gameControl.reset()
     }
+}
+
+class MatchingButton: UIButton {
+    var x = 0
+    var y = 0
+    var connectionTo: (Int, Int)? = nil
 }
